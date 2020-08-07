9aae0bdebb74dc2f5eefad5108b3f87cda5d58c3
yangbin <1175019761@qq.com>
2020-08-07 17:51:11 +0800

    fisher start

---
+++ fisher.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/8/5 16:12
+# @Author  : yang
+# @File    : fisher.py
+from app import create_app
+
+app = create_app()
+
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=81,debug=app.config['DEBUG'])
